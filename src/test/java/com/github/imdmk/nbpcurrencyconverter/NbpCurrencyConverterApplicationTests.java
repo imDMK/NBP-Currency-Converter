3e56097c64a8ad1b4960301873036af9e6855039
imDMK <dominiks8318@gmail.com>
2026-03-15 12:25:30 +0100

    chore: initialize spring boot project

---
+++ src/test/java/com/github/imdmk/nbpcurrencyconverter/NbpCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.imdmk.nbpcurrencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpCurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
